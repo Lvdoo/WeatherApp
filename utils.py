7978009b127cd75d9d7cca5b02d1a1cadb8e0482
Ludovic Li <laohu.ludo@gmail.com>
2025-12-22 23:05:01 +0100

    Added comments

--- utils.py
+++ utils.py
@@ -1,6 +1,17 @@
 from datetime import datetime 
 
 def to_time(timestamp : int, timezone : int) -> datetime :
+    """
+    Args:
+        timestamp (int): timestamp of time 
+        timezone (int): timestamp of time difference
+
+    Raises:
+        ValueError: message of error if values are wrong
+
+    Returns:
+        datetime: time in datetime 
+    """
     if timestamp is not None and timezone is not None :
         dt = datetime.fromtimestamp(timestamp + timezone)
         return dt
@@ -8,6 +19,16 @@ def to_time(timestamp : int, timezone : int) -> datetime :
         raise ValueError("timestamp and timezone must be integers")
 
 def wind_deg_to_direction(wind_deg : float) -> str :
+    """
+    Args:
+        wind_deg (float): degree of wind
+
+    Raises:
+        ValueError: message of error if value is wrong
+
+    Returns:
+        str: return wind in form "N","NE","NW","S","SE","Sw","E",W"
+    """
     if wind_deg < 22.5 or (wind_deg > 337.5 and wind_deg <= 360) :
         return "N"
     elif wind_deg >= 22.5 and wind_deg < 67.5 :
