afa04104bd24ab9b49febf8e3521e2c25d55e32c
Ludovic Li <laohu.ludo@gmail.com>
2026-01-10 22:25:12 +0100

    Get weather for next 3 days every 3 hours

--- weather_api.py
+++ weather_api.py
@@ -41,3 +41,68 @@ def get_weather(city : str, unit : str) -> dict:
         "timezone": data["timezone"]
     }
     return weather_dict
+
+def geolocalisation(city : str) -> str : 
+    """
+    Get latitude and logitude of a city
+
+    Args:
+        city (str): Name of the city
+
+    Returns:
+        str: Latitude and logitude of the city
+    """
+
+    if not city or not isinstance(city, str):
+        raise ValueError("City must be a non-empty string")
+    url = f"https://api.openweathermap.org/geo/1.0/direct?q={city}&appid={APIKEY}"
+    response = requests.get(url)
+    data = response.json()
+
+    if response.status_code != 200 :  #http code = 200 : everything ok
+        raise ValueError(f"API error: {data.get('message', 'Unknown error')}")
+    
+    latitude = data[0]["lat"]
+    longitude = data[0]["lon"]
+
+    return latitude, longitude
+
+def get_previsional_weather(city : str) -> dict :
+    """
+    Get the weather for the next 3 days
+    Args:
+        latitude (str): latitude of the city for the weather
+        longitude (str): longitude of the city for the weather
+
+    Returns:
+        dict: Return a dictionnary of infos about weather of a city
+    """
+    weather_prevision_dict = {}
+    latitude, longitude = geolocalisation(city)
+    
+    if not latitude is None or longitude is None:
+        raise ValueError("City must be a non-empty string")
+    
+    url = f"https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon=2.{longitude}&appid={APIKEY}"
+    response = requests.get(url)
+    data = response.json()
+
+    if response.status_code != 200 :  #http code = 200 : everything ok
+        raise ValueError(f"API error: {data.get('message', 'Unknown error')}")
+    
+    hourly_weather = {}
+
+    for forecast in data["list"]:
+        hour = forecast["dt_txt"] 
+
+        hourly_weather[hour] = {
+            "temp": forecast["main"]["temp"],
+            "temp_min": forecast["main"]["temp_min"],
+            "temp_max": forecast["main"]["temp_max"],
+            "weather": forecast["weather"][0]["main"],
+            "icon": forecast["weather"][0]["icon"],
+            "wind": forecast["wind"]["speed"],
+        }
+
+    return hourly_weather
+
